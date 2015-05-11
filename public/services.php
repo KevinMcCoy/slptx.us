312feada445b1c3f71bb4fbe8a9165c3e39226ff
Kevin McCoy <KevinMcCoy@earthlink.net>
2015-05-11 07:21:18 -0400

    Add two image and adj font

--- public/services.php
+++ public/services.php
@@ -14,9 +14,11 @@
 					<h1>Articulation Disorder/Delay:</h1>
 				</header>
 				<section>
-					<p>Is when an speech sound is produced incorrectly. 
-					This makes it difficult to be understood and can hinder 
+					<img src='<?= (baseurl());?>img/IMG_Nav1a.jpg' alt='' width='19%' height='' style="float:right; margin:0px 25px;">
+					<p style="width:64%;">Is when an speech sound is produced incorrectly.
+					This makes it difficult to be understood and can hinder
 					learning to read.</p>
+					<div style="clear:both;"></div>
 				</section>
 			</article>
 			<article>
@@ -24,10 +26,10 @@
 					<h1>Language Disorder/Delay:</h1>
 				</header>
 				<section>
-					<p>Receptive language refers to the ability to understand 
-			what is being said. You are receiving the information. 
-			Expressive language refers to the ability to say what 
-			you are thinking. You are expressing what you are 
+					<p>Receptive language refers to the ability to understand
+			what is being said. You are receiving the information.
+			Expressive language refers to the ability to say what
+			you are thinking. You are expressing what you are
 			thinking.</p>
 				</section>
 			</article>
@@ -36,9 +38,9 @@
 					<h1>Fluency/Stuttering:</h1>
 				</header>
 				<section>
-					<p>Occurs when sounds, syllables, or words are 
-			repeated or prolonged. This causes an interference in 
-			the normal flow of speech, which makes it difficult for 
+					<p>Occurs when sounds, syllables, or words are
+			repeated or prolonged. This causes an interference in
+			the normal flow of speech, which makes it difficult for
 			the listener to concentrate on what is being said.</p>
 				</section>
 			</article>
@@ -47,13 +49,13 @@
 					<h1>Phonological Impairment:</h1>
 				</header>
 				<section>
-					<p>Involves incorrect speech patterns. Typically, these 
-			errors could include leaving the final consonant 
-			off most words: "ha for hat," or "cu for cup." Or 
-			substituting consonant 
-			sounds that are made in the front of the mouth such 
-			as "t" and "d" for sounds that are made in the back 
-			of the mouth such as "g" and "k". So the client might 
+					<p>Involves incorrect speech patterns. Typically, these
+			errors could include leaving the final consonant
+			off most words: "ha for hat," or "cu for cup." Or
+			substituting consonant
+			sounds that are made in the front of the mouth such
+			as "t" and "d" for sounds that are made in the back
+			of the mouth such as "g" and "k". So the client might
 			say "dood for good" or "tate for cake."</p>
 				</section>
 			</article>
@@ -62,10 +64,12 @@
 					<h1>Oral-motor Exercises:</h1>
 				</header>
 				<section>
-					<p>Refers to the strength and coordination of the oral/
-			facial muscles needed for correct speech. The client may 
-			exhibit drooling, chewing on clothes, or breathing 
-			through their mouth.</p>
+					<img src='<?= (baseurl());?>img/IMG_Nav1b.jpg' alt='' width='50%' height='' style="float:right; margin:0px 25px;">
+					<p style="width:45%;">Refers to the strength and coordination of the oral/
+					facial muscles needed for correct speech. The client may
+					exhibit drooling, chewing on clothes, or breathing
+					through their mouth.</p>
+					<div style="clear:both;"></div>
 				</section>
 			</article>
 		</div><!-- [END]#accordion -->
@@ -86,8 +90,8 @@
 			That way, the child will improve at a faster rate and the parents are happier!
 		</p>
 
-		<span>SPEECH AND LANGUAGE THERAPY IS AVAILABLE 
-			FOR CHILDREN/ADULTS EXPERIENCING 
+		<span>SPEECH AND LANGUAGE THERAPY IS AVAILABLE
+			FOR CHILDREN/ADULTS EXPERIENCING
 			DIFFICULTY IN THE FOLLOWING AREAS:
 		</span>
 
@@ -96,7 +100,3 @@
 
 </body>
 </html>
-
-
-
-
