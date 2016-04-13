d2e67f5a67d517696da4e2bea494ea63e13651fa
Kevin McCoy <kevinmccoy@earthlink.net>
2016-04-12 23:27:01 -0400

    More options added to rollbar.js sh

--- deployIntegration.sh
+++ deployIntegration.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
-clear
+# clear
 
 echo "bin bash starting"
 
 ACCESS_TOKEN=376309144d3f4f5c89e1162015e73990
 ENVIRONMENT=production
+REVISION=git log -n 1 --pretty=format:"%H"
+COMMENT=Test
 LOCAL_USERNAME='KevinMcCoy'
-REVISION=`git log -n 1 --pretty=format:"%H"`
 
 curl https://api.rollbar.com/api/1/deploy/ \
 	-F access_token=$ACCESS_TOKEN \
 	-F environment=$ENVIRONMENT \
 	-F revision=$REVISION \
+	-F comment=$COMMENT \
 	-F local_username=$LOCAL_USERNAME
+	-F rollbar_username=$LOCAL_USERNAME
