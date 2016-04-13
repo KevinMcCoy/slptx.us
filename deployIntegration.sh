99c98e3451f3a5975c6c5f2a5f746622df5ef5d6
Kevin McCoy <kevinmccoy@earthlink.net>
2016-04-12 23:28:55 -0400

    error on 18

--- deployIntegration.sh
+++ deployIntegration.sh
@@ -7,7 +7,8 @@ ACCESS_TOKEN=376309144d3f4f5c89e1162015e73990
 ENVIRONMENT=production
 REVISION=git log -n 1 --pretty=format:"%H"
 COMMENT=Test
-LOCAL_USERNAME='KevinMcCoy'
+LOCAL_USERNAME=KevinMcCoy
+ROLLBAR_USERNAME=KevinMcCoy
 
 curl https://api.rollbar.com/api/1/deploy/ \
 	-F access_token=$ACCESS_TOKEN \
@@ -15,4 +16,4 @@ curl https://api.rollbar.com/api/1/deploy/ \
 	-F revision=$REVISION \
 	-F comment=$COMMENT \
 	-F local_username=$LOCAL_USERNAME
-	-F rollbar_username=$LOCAL_USERNAME
+	-F rollbar_username=$ROLLBAR_USERNAME
