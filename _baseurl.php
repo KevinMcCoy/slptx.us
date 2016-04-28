bb1a1fde355b90670b6c23bbd85f3b9e4fed7ab1
Kevin McCoy <KevinMcCoy@earthlink.net>
2016-04-28 01:04:56 -0400

    Run analyticstracking only on live server

--- _baseurl.php
+++ _baseurl.php
@@ -1,15 +1,18 @@
 <?php
-	function baseURL($folder){
+	function baseURL($input){
 
 		//$base = $_SERVER['SERVER_NAME']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
 		$base = $_SERVER['HTTP_HOST']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
 		//$name = $_SERVER['REQUEST_URI']; //string(29) "/slptx.us/public/services.php" <--> string(1) "/"
 
 
-		// Print Base URL
-
-		if($folder){
-			return $folder.'/';
+		//  Check if running live
+		if($input == 'live'){
+			if($_SERVER['HTTP_HOST'] !== 'localhost' AND $_SERVER['HTTP_HOST'] !== '127.0.0.1'){
+				return true;
+			}else{
+				return false;
+			}
 		}
 
 		// return basename($name).'/'; //
--- public/inc/footer.php
+++ public/inc/footer.php
@@ -1,7 +1,12 @@
+<!-- footer -->
 <footer id="" role="">
 	<a class="i" href="https://www.facebook.com/MaitlandSpeechTherapy" target="_blank">
 		<i class="fa fa-facebook-square"></i>
 	</a>
 	<div id="">Maitland Speech Therapy <span>&copy;2015</span></div>
 </footer>
-<?php include_once("vendors/analyticstracking.php") ?>
+
+<?php if( baseURL('live') ){
+	include_once("vendors/analyticstracking.php");
+} ?>
+<!-- [END] footer -->
