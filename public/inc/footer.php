44129d68adedeb3e83e5098419118a02b0edafb1
Kevin McCoy <KevinMcCoy@earthlink.net>
2019-12-20 00:47:08 -0500

    Update all to homepage

--- public/inc/footer.php
+++ public/inc/footer.php
@@ -1,11 +1,13 @@
 <!-- footer -->
 <footer class="box">
+	<a class="call" href="tel:4079009859">407-900-9859</a>
+
 	<a class="i" href="https://www.facebook.com/MaitlandSpeechTherapy" target="_blank">
 		<i class="fa fa-facebook-square"></i>
 	</a>
 	<div>Maitland Speech Therapy <span>&copy;2015 - <?= date(Y); ?></span></div>
 
-	<img src='<?= (baseurl());?>img/ashaLogoBW.png' alt='' width='180' height='65'>
+	<!-- <img src='<?//= (baseurl());?>img/ashaLogoBW.png' alt='' width='180' height='65'> -->
 
 </footer>
 
