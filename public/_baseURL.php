53b1451000af096e189e0c5d770b2c1c43d5038a
KevinMcCoy <kevinwaterskier@gmail.com>
2023-08-28 20:05:07 -0400

    baseURL function name change from live to check_if_live

--- public/_baseURL.php
+++ public/_baseURL.php
@@ -1,25 +1,24 @@
 <?php
 
-	// This function does one of two things:
-	// #1) Returns base url path
-	// #2) Check if currently running on a live server and returns true/false
-	function baseURL($input=false){
+// This function does one of two things:
+// #1) Returns base url path
+// #2) Check if currently running on a live server and returns true/false
+function baseURL($input = false)
+{
 
-		//$base = $_SERVER['SERVER_NAME']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
-		$base = $_SERVER['HTTP_HOST']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
-		//$name = $_SERVER['REQUEST_URI']; //string(29) "/slptx.us/public/services.php" <--> string(1) "/"
+    //$base = $_SERVER['SERVER_NAME']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
+    $base = $_SERVER['HTTP_HOST']; //string(9) "127.0.0.1"											<--> string(12) "dev.slptx.us"
+    //$name = $_SERVER['REQUEST_URI']; //string(29) "/slptx.us/public/services.php" <--> string(1) "/"
 
-		// #2 - Check if running live
-		if($input == 'live'){
-			if($_SERVER['HTTP_HOST'] !== 'localhost' AND $_SERVER['HTTP_HOST'] !== '127.0.0.1'){
-				return true;
-			}else{
-				return false;
-			}
-		}
+    // #2 - Check if running live
+    if ($input == 'check_if_live') {
+        if ($_SERVER['HTTP_HOST'] !== 'localhost' and $_SERVER['HTTP_HOST'] !== '127.0.0.1') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-		// #1 - Returns base url path
-		return dirname($base).'/';
-	}
-
-?>
+    // #1 - Returns base url path
+    return dirname($base) . '/';
+}
--- public/inc/footer.php
+++ public/inc/footer.php
@@ -1,17 +1,17 @@
 <!-- footer -->
 <footer class="box">
-	<a class="call" href="tel:4079009859">407-900-9859</a>
+    <a class="call" href="tel:4079009859">407-900-9859</a>
 
-	<a class="i" href="https://www.facebook.com/MaitlandSpeechTherapy" target="_blank">
-		<i class="fa fa-facebook-square"></i>
-	</a>
-	<div>Maitland Speech Therapy <span>&copy;2015 - <?= date('Y'); ?></span></div>
+    <a class="i" href="https://www.facebook.com/MaitlandSpeechTherapy" target="_blank">
+        <i class="fa fa-facebook-square"></i>
+    </a>
+    <div>Maitland Speech Therapy <span>&copy;2015 - <?= date('Y'); ?></span></div>
 
-	<!-- <img src='<?//= (baseurl());?>img/ashaLogoBW.png' alt='' width='180' height='65'> -->
+    <!-- <img src='<?//= (baseurl());?>img/ashaLogoBW.png' alt='' width='180' height='65'> -->
 
 </footer>
 
-<?php if( baseURL('live') ){
-	include_once("vendors/analyticstracking.php");
-}?>
+<?php if (baseURL('check_if_live')) {
+    include_once("vendors/analyticstracking.php");
+} ?>
 <!-- [END] footer -->
